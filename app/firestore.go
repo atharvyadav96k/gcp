4b6f3d2fe7a57c98cdf633862aadcfa78688a17d
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2026-04-18 19:00:32 +0530

    added support of interfaces instead of struct for more flexibilty

--- app/firestore.go
+++ app/firestore.go
@@ -4,18 +4,18 @@ import (
 	"context"
 )
 
-func (a *App) StoreCreate(ctx context.Context, collection string, data struct{}) error {
+func (a *App) StoreCreate(ctx context.Context, collection string, data interface{}) error {
 	_, _, err := a.StoreDoc(collection).Add(ctx, data)
 	return err
 }
 
-func (a *App) StoreCreateWithId(ctx context.Context, collection string, id string, data struct{}) error {
+func (a *App) StoreCreateWithId(ctx context.Context, collection string, id string, data interface{}) error {
 	docRef := a.StoreDoc(collection).Doc(id)
 	_, err := docRef.Create(ctx, data)
 	return err
 }
 
-func (a *App) StoreUpdate(ctx context.Context, collection string, id string, data struct{}) error {
+func (a *App) StoreUpdate(ctx context.Context, collection string, id string, data interface{}) error {
 	docRef := a.StoreDoc(collection).Doc(id)
 	_, err := docRef.Set(ctx, data)
 	return err
