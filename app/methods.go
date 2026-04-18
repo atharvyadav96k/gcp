fc63fcd7b35e4e6999e6b8de47dccf15e23ed92b
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2026-04-18 17:25:37 +0530

    Store Curd Operation

--- app/methods.go
+++ app/methods.go
@@ -1,7 +1,13 @@
 package app
 
-import "cloud.google.com/go/firestore"
+import (
+	"cloud.google.com/go/firestore"
+)
 
-func (a *App) Database() *firestore.Client {
+func (a *App) Store() *firestore.Client {
 	return a.FireStore.FirestoreClient
+}
+
+func (a *App) StoreDoc(collection string) *firestore.CollectionRef {
+	return a.FireStore.FirestoreClient.Collection(collection)
 }
